cd29b62e907e7fed17248c1067fff5e0067fc0f3
Nisha Pillai <grnisha@users.noreply.github.com>
2024-10-11 09:58:58 +0000

    Updated api ti use SK

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -47,17 +47,17 @@ builder.Services.AddSingleton<Kernel>((_) =>
         apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
     );
     kernelBuilder.Plugins.AddFromType<DatabaseService>();
+    #pragma warning disable SKEXP0010
+    kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(
+    deploymentName: builder.Configuration["AzureOpenAI:EmbeddingDeploymentName"]!,
+    endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+    apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+    );
+    #pragma warning restore SKEXP0010 
     return kernelBuilder.Build();
 });
 
-builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
-{
-    var endpoint = new Uri(builder.Configuration["AzureOpenAI:Endpoint"]!);
-    var credentials = new AzureKeyCredential(builder.Configuration["AzureOpenAI:ApiKey"]!);
 
-    var client = new AzureOpenAIClient(endpoint, credentials);
-    return client;
-});
 
 var app = builder.Build();
 
